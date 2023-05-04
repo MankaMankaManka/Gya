a2c6b106fd523bd810ebaa6028ef09ff8e1b4f3c
MankaMankaManka <109943723+MankaMankaManka@users.noreply.github.com>
2023-05-04 16:44:59 +0930

    Create LinkedList.cpp

---
+++ LinkedList.cpp
@@ -0,0 +1,18 @@
+// Header files
+#include "LinkedList.h"
+#include <iostream>
+
+LinkedList::LinkedList(){
+    new LinkedList;
+    
+}
+
+LinkedList::LinkedList(int* array, int len){
+    for (int i=0;i<len;i++){
+        Node::*new_node = new Node node;
+
+    new_node->data = data;
+        
+    }
+    
+}
